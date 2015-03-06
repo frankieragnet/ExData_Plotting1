f329b540e261bda7b94491613d120a3ac277f095
Frankie Ragnet <Francois.Ragnet@xerox.com>
2015-03-06 22:46:59 +0100

    updated

--- plot1.R
+++ plot1.R
@@ -1,6 +1,5 @@
 plot1 <- function()
 {
-  ##RE COMMENT OUT
   download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile="./PowerConsumption.zip")
   unzip("PowerConsumption.zip")
   
